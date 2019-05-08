c3dfa623d8c446b2bb137615c9f31d8475b49204
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-08 16:40:56 +0200

    refactor: fix visibility

--- main.go
+++ main.go
@@ -17,8 +17,7 @@ import (
 	shellquote "github.com/kballard/go-shellquote"
 )
 
-// ResultArtifactPathPattern is a glob pattern matching test result XMLs.
-const ResultArtifactPathPattern = "*TEST*.xml"
+const resultArtifactPathPattern = "*TEST*.xml"
 
 // Configs ...
 type Configs struct {
@@ -215,7 +214,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	resultXMLs, err := getArtifacts(gradleProject, started, ResultArtifactPathPattern, false)
+	resultXMLs, err := getArtifacts(gradleProject, started, resultArtifactPathPattern, false)
 	if err != nil {
 		failf("Failed to find test result XMLs, error: %v", err)
 	}
