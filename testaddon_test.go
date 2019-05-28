e6815b3da183ed9dea409f709d2e83ef9378e6d7
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-05-28 13:44:15 +0200

    initial failing test

---
+++ testaddon_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetUniqueDir(t *testing.T) {
+	tc := []struct{
+		title string
+		path string
+		wantStr string
+		isErr bool
+	}{
+		{
+			title: "should return error on empty string",
+			path: "",
+			wantStr: "",
+			isErr: true, 
+		},
+		{
+			title: "should return error if artifact path ends in test results folder with trailing slash",
+			path: "/path/to/test-results/",
+			wantStr: "",
+			isErr: true, 
+		},
+	}
+	
+	for _, tt := range tc {
+		str, err := getUniqueDir(tt.path)
+		if str != tt.wantStr || (err != nil) != tt.isErr {
+			t.Fatalf("%s: got (%s, %s)", tt.title, str, err)
+		}
+	}
+
+}
