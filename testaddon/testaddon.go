4e0817809e35eae3fc82e1935918ddea16af45f9
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-11-06 16:58:28 +0100

    go fmt

--- testaddon/testaddon.go
+++ testaddon/testaddon.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// ResultDescriptorFileName is the name of the test result descriptor file.
-	ResultDescriptorFileName  = "test-info.json"
+	ResultDescriptorFileName = "test-info.json"
 )
 
 func generateTestInfoFile(dir string, data []byte) error {
@@ -38,26 +38,26 @@ func generateTestInfoFile(dir string, data []byte) error {
 // ExportArtifact exports artifact found at path in directory uniqueDir,
 // rooted at baseDir.
 func ExportArtifact(path, baseDir, uniqueDir string) error {
-		exportDir := filepath.Join(baseDir, uniqueDir)
+	exportDir := filepath.Join(baseDir, uniqueDir)
 
-		if err := os.MkdirAll(exportDir, os.ModePerm); err != nil {
-			return fmt.Errorf("skipping artifact (%s): could not ensure unique export dir (%s): %s", path, exportDir, err)
-		}
+	if err := os.MkdirAll(exportDir, os.ModePerm); err != nil {
+		return fmt.Errorf("skipping artifact (%s): could not ensure unique export dir (%s): %s", path, exportDir, err)
+	}
 
-		if _, err := os.Stat(filepath.Join(exportDir, ResultDescriptorFileName)); os.IsNotExist(err) {
-			m := map[string]string{"test-name": uniqueDir}
-			data, err := json.Marshal(m)
-			if err != nil {
-				return fmt.Errorf("create test info descriptor: json marshal data (%s): %s", m, err)
-			}
-			if err := generateTestInfoFile(exportDir, data); err != nil {
-				return fmt.Errorf("create test info descriptor: generate file: %s", err)
-			}
+	if _, err := os.Stat(filepath.Join(exportDir, ResultDescriptorFileName)); os.IsNotExist(err) {
+		m := map[string]string{"test-name": uniqueDir}
+		data, err := json.Marshal(m)
+		if err != nil {
+			return fmt.Errorf("create test info descriptor: json marshal data (%s): %s", m, err)
 		}
+		if err := generateTestInfoFile(exportDir, data); err != nil {
+			return fmt.Errorf("create test info descriptor: generate file: %s", err)
+		}
+	}
 
-		name := filepath.Base(path)
-		if err := command.CopyFile(path, filepath.Join(exportDir, name)); err != nil {
-			return fmt.Errorf("failed to export artifact (%s), error: %v", name, err)
-		}
+	name := filepath.Base(path)
+	if err := command.CopyFile(path, filepath.Join(exportDir, name)); err != nil {
+		return fmt.Errorf("failed to export artifact (%s), error: %v", name, err)
+	}
 	return nil
 }
